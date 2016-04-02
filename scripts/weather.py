3ab146c0128b240f48fceacf579c0dc908a4e817
Chris Spooner <cmspooner@gmail.com>
2016-04-02 11:57:53 -0400

    fix?

--- scripts/weather.py
+++ scripts/weather.py
@@ -2,7 +2,7 @@
 
 import os, sys
 
-debug = False
+debug = False 
 
 os.system('curl -s "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22concord%2Cnh%22)&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys" -o ~/.cache/temp_weather.xml')
 
